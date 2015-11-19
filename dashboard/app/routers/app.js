3c84302d0940283f21867c5f221d93b1f8ae5a72
Hyeungshik Jung <hyungshike@gmail.com>
2015-11-19 11:27:29 +0900

    sample routing

--- dashboard/app/routers/app.js
+++ dashboard/app/routers/app.js
@@ -1,13 +1,15 @@
 'use strict';
 
-import { Router } from 'backbone'
+import { Router, history } from 'backbone'
 import app from '../app'
 import RootView from '../views/root'
+import ConfigView from '../views/config'
 
 export default class AppRouter extends Router {
   constructor(options) {
     super(options)
     this.routes = {
+      "config": "setting",
       '': 'root'
     }
     this._bindRoutes()
@@ -20,5 +22,10 @@ export default class AppRouter extends Router {
 
   root() {
     this.setView(new RootView().render())
+  }
+
+  setting() {
+    console.log("setting fired")
+    this.setView(new ConfigView().render())
   }
 }

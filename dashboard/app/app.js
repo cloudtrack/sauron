179f8b83e673d0452a106d30852863282b152a0a
Hoseong Hwang <thefron@ultracaption.net>
2015-12-06 21:03:47 +0900

    Put es client into global app object

--- dashboard/app/app.js
+++ dashboard/app/app.js
@@ -1,2 +1,6 @@
-var app = {}
+import es from 'elasticsearch'
+var esClient = new es.Client({ host: ELASTICSEARCH_HOST })
+var app = {
+  es: esClient
+}
 module.exports = app
--- dashboard/app/es.js
+++
@@ -1,5 +0,0 @@
-import es from 'elasticsearch'
-
-var client = new es.Client({ host: ELASTICSEARCH_HOST })
-
-export default client

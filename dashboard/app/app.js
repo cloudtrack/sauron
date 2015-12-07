e838056c177f0e66f931ad05f7107db204a5ff2f
Hoseong Hwang <thefron@ultracaption.net>
2015-12-07 12:46:52 +0900

    Replace es client with jquery wrapped version

--- dashboard/app/app.js
+++ dashboard/app/app.js
@@ -1,5 +1,6 @@
-import es from 'elasticsearch'
-var esClient = new es.Client({ host: ELASTICSEARCH_HOST })
+import $ from 'jquery'
+require('imports?jQuery=jquery!elasticsearch/src/elasticsearch.jquery')
+var esClient = new $.es.Client({ host: ELASTICSEARCH_HOST })
 var app = {
   es: esClient
 }

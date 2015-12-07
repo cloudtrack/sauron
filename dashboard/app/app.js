f67d4e67b418aa2e35905fdce28dd84020c852cc
Hoseong Hwang <thefron@ultracaption.net>
2015-12-07 13:57:40 +0900

    Set ES apiVersion to 1.5(To match apiVersion of Elasticsearch Service)

--- dashboard/app/app.js
+++ dashboard/app/app.js
@@ -1,6 +1,9 @@
 import $ from 'jquery'
 require('imports?jQuery=jquery!elasticsearch/src/elasticsearch.jquery')
-var esClient = new $.es.Client({ host: ELASTICSEARCH_HOST })
+var esClient = new $.es.Client({
+  host: ELASTICSEARCH_HOST,
+  apiVersion: '1.5'
+})
 var app = {
   es: esClient
 }

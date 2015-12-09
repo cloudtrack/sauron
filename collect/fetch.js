4a9064043d7bbe5ff5068b0460ca6aba7262a7e8
Hyeungshik Jung <hyungshike@gmail.com>
2015-12-10 02:13:17 +0900

    index one metric

--- collect/fetch.js
+++ collect/fetch.js
@@ -4,6 +4,12 @@ var aws = require('aws-sdk');
 aws.config.update({region: 'ap-northeast-1'});
 var CloudWatch = new aws.CloudWatch({apiVersion: '2010-08-01'});
 
+var ES = require('elasticsearch');
+var client = new ES.Client({
+  host: 'https://search-sauron-j75hyivu5hfn4sayixpwx6gmru.ap-northeast-1.es.amazonaws.com',
+  log: 'trace'
+});
+
 exports.handler = function(event, context) {
   var params = {
     StartTime: new Date(new Date().setTime(new Date().getTime() - 5*60*1000)),
@@ -16,9 +22,27 @@ exports.handler = function(event, context) {
   };
 
  CloudWatch.getMetricStatistics(params, function(err, data) {
-  if (err)
-    console.log(err, err.stack);
-  else
-    console.log(data);
+    if (err)
+      console.log(err, err.stack);
+    else {
+      console.log(data);
+      client.create({
+        index: 'services',
+        //http://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/aws-namespaces.html
+        //we can omit 'AWS/' parts
+        type: params.Namespace.split('/')[1],
+        body: {
+          date: data.Datapoints[0].Timestamp,
+          metric: params.MetricName,
+          value: data.Datapoints[0].Average
+        }
+      }, function (error, response) {
+        if (err) {
+          console.log(error);
+        } else {
+          // console.log(response);
+        }
+      })
+    }
   });
 }

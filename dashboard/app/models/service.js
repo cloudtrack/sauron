cdf274e90efdf1a423f1546fb8e21ec7f715d4a7
Hoseong Hwang <thefron@ultracaption.net>
2015-12-14 16:59:44 +0900

    Fetch EC2 instances tagged with serviceId

--- dashboard/app/models/service.js
+++ dashboard/app/models/service.js
@@ -1,8 +1,27 @@
 import ESModel from './es'
+import _ from 'lodash'
 
 class ServiceModel extends ESModel {
   constructor(options) {
     super(options)
+
+    this.bind('save', this.fetchAWSResources)
+  fetchAWSResources () {
+    var that = this
+    var ec2 = new AWS.EC2()
+    ec2.describeInstances({
+      Filters: [
+        {
+          Name: 'tag:serviceId',
+          Values: [this.get('name')]
+        }
+      ]
+    }, function(err, data) {
+      if (err) {
+        console.log('Failed to fetch EC2 Instances')
+        return
+      }
+    })
   }
 }
 

31a74dad9a31d2a1d8a1617091ba7d4cfcd3fb97
Hyeungshik Jung <hyungshike@gmail.com>
2015-12-16 04:50:52 +0900

    add preset range buttons and some codes..but not working

--- dashboard/app/views/chartBig.js
+++ dashboard/app/views/chartBig.js
@@ -1,10 +1,20 @@
 import Backbone from 'backbone'
 import Chart from 'chart.js'
+import getLog from '../data/getLogs'
+import conv from '../data/chartjsDataConverter'
 
 module.exports = Backbone.View.extend({
   template: require('templates/chart_big_view.hbs'),
 
   className: 'chartBox',
+
+  events: {
+    'click .preset' : 'showPreset'
+  },
+
+  initialize: function() {
+    _.bindAll(this, 'showPreset')
+  },
 
   render: function() {
     this.$el.html(this.template({ title: this.model.get('title') }))
@@ -15,5 +25,22 @@ module.exports = Backbone.View.extend({
     var ctx = this.$('.chart-container').get(0).getContext("2d")
     var data = this.model.get('data')
     var myLineChart = new Chart(ctx).Line(data)
+  },
+
+  showPreset: function(evt) {
+    evt.preventDefault()
+    var callback = function(data) {
+      console.log(this)
+    }
+    _.bindAll(this, callback)
+
+    getLog('EC2', 'CPUUtilization', String(evt.target.id), function(data) {
+      callback(data);
+      // console.log(this)
+      // this.model.set({ data: conv(data.label, data.value) })
+      // this.render().drawChart();
+    }, function(err) {
+      console.log(err)
+    });
   }
 })

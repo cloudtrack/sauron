1382e1d97bc120e606a5160521cda427cf6ebb29
Hoseong Hwang <thefron@ultracaption.net>
2015-12-17 05:08:28 +0900

    Draw chart per metric using MetricModel

--- dashboard/app/models/metric.js
+++ dashboard/app/models/metric.js
@@ -1,12 +1,22 @@
 import Backbone from 'backbone'
-var sampleData = require('json!../data/sample.json');
+import conv2chartjs from '../data/chartjsDataConverter'
+import logQuery from '../data/getLogs'
 
 module.exports = Backbone.Model.extend({
-	defaults: {
-		data: sampleData,
-		state: '',
-		title: 'Default title',
-		id: 0,
-		unit: "%"
-	}
+  defaults: {
+    data: null,
+    metricName: null,
+    instanceType: null,
+    unit: "%"
+  },
+
+  fetch: function() {
+    var that = this
+    logQuery(this.get('instanceType'), this.get('metricName'), '6h', function(result) {
+      that.set('data', conv2chartjs(result.label, result.value))
+      that.trigger('loaded')
+    }, function (err) {
+      console.log(err.message)
+    })
+  }
 });
--- dashboard/app/views/metric.js
+++ dashboard/app/views/metric.js
@@ -1,4 +1,6 @@
 import { View } from 'backbone'
+import MetricModel from '../models/metric'
+import Chart from 'chart.js'
 
 export default View.extend({
   tagName: 'div',
@@ -6,6 +8,9 @@ export default View.extend({
   initialize: function (options) {
     this.metricName = options.metricName
     this.instanceType = options.instanceType
+    this.model = new MetricModel({ metricName: this.metricName, instanceType: this.instanceType })
+    this.model.fetch()
+    this.listenTo(this.model, 'loaded', this.drawChart)
   },
 
   template: require('../templates/metric_view.hbs'),
@@ -16,5 +21,11 @@ export default View.extend({
       instanceType: this.instanceType
     }))
     return this
+  },
+
+  drawChart: function() {
+    var ctx = this.$('.chart-container').get(0).getContext('2d')
+    var data = this.model.get('data')
+    var myLineChart = new Chart(ctx).Line(data)
   }
 })
